a9e7c7e7d6dffc82d92d1d9b52f68653d0e70111
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-07-11 15:09:44 +0700

    update

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    AOS.init()
+})
+
+
+
+// $(".data-detail").hide()
+$(".gambar").click(function(){
+    var id_ = $(this).attr('id')
+    var show = $("#show-"+id_+"");
+//    show.removeAttr('hidden')
+    show.fadeIn()
+})
+
+$(".tutups").click(function(){
+    $(this).parent().fadeOut()
+})
